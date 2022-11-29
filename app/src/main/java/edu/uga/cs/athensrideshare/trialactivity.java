d8cffa22c81d7303507959f468ee961ae46b6f70
Manmeet1808 <55161847+Manmeet1808@users.noreply.github.com>
2022-11-28 21:58:04 -0500

    Initial commit

---
+++ app/src/main/java/edu/uga/cs/athensrideshare/trialactivity.java
@@ -0,0 +1,14 @@
+package edu.uga.cs.athensrideshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class trialactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trialactivity);
+    }
+}
